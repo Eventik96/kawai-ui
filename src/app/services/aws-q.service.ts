0a4256b38c08a6cc9a29bb072026206c7dfb9145
Ábel Keresztes-Nagy <knabel1996@gmail.com>
2025-04-16 14:52:26 +0200

    Update aws-q.service.ts

--- src/app/services/aws-q.service.ts
+++ src/app/services/aws-q.service.ts
@@ -20,6 +20,7 @@ export class AWSQService {
             if(data?.text) {
               let text = data?.text.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
               text = text.replace(/\\n/g, '<br>');
+              text = text.replace(/▔/g, '');
               observer.next(text);
             }
           }
